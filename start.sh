1f6b4ff715b3d86feb683ab9383009aaed356053
bonmassarivan <ivan.bonmassar@protonmail.com>
2021-06-06 14:14:07 +0000

    added script

---
+++ start.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+helm delete mongo
+helm delete frontend
+kubectl delete pvc mongo-pvc
+
+helm install mongo ./mongodb
+helm install frontend ./application
+
+echo "Sleeping for 20"
+sleep 20
+kubectl port-forward svc/frontend 8080:80 &
+
